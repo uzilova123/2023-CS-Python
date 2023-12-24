e2a08763ecb82afcf648a6f6793a1cd20f4e9e94
uzilova123 <uzilova12@mail.ru>
2023-12-24 20:57:58 +1000

    Update singleton.py

--- src/03_Singleton/singleton.py
+++ src/03_Singleton/singleton.py
@@ -1,9 +1,7 @@
-class Singleton(object):
+class Singleton:
     instance = None
-    def __new__(class_, *args, **kwargs):
-        if not isinstance(class_.instance, class_):
-            class_.instance = object.__new__(class_, *args, **kwargs)
-        return class_.instance
 
-
-
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls.instance, cls):
+            cls.instance = object.__new__(cls, *args, **kwargs)
+        return cls.instance
