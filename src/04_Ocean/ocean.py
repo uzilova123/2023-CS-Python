0ff843659841466ff0faad987b989eb039374ab4
uzilova123 <uzilova12@mail.ru>
2024-01-11 10:50:01 +1000

    Update ocean.py

--- src/04_Ocean/ocean.py
+++ src/04_Ocean/ocean.py
@@ -9,7 +9,7 @@ class Ocean:
         self.state = init_state
 
     def __str__(self) -> str:
-        return "\n".join(["".join(str(el) for el in row) for row in self.state])
+        return "\n".join([" ".join(str(el) for el in row) for row in self.state])
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.state!r})"
@@ -19,52 +19,50 @@ class Ocean:
         shrimp = 3
         new_state = []
         for i in range(len(self.state)):
-            new_r = []
-            for k in range(len(self.state[i])):
-                if self.state[i][k] == 1:
-                    new_r.append(1)
+            new_row = []
+            for j in range(len(self.state[i])):
+                if self.state[i][j] == 1:
+                    new_row.append(1)
                 else:
-                    k_fish = 0
-                    k_shrimp = 0
-                    sosed = [
-                        (i - 1, k - 1),
-                        (i - 1, k),
-                        (i - 1, k + 1),
-                        (i, k - 1),
-                        (i, k + 1),
-                        (i + 1, k - 1),
-                        (i + 1, k),
-                        (i + 1, k + 1),
+                    n_fish = 0
+                    n_shrimp = 0
+                    neighbors = [
+                        (i - 1, j - 1),
+                        (i - 1, j),
+                        (i - 1, j + 1),
+                        (i, j - 1),
+                        (i, j + 1),
+                        (i + 1, j - 1),
+                        (i + 1, j),
+                        (i + 1, j + 1),
                     ]
-                    for n_i, n_k in sosed:
-                        if n_i < 0 or n_k < 0 or n_i >= len(self.state) or n_k >= len(self.state[i]):
+                    for ni, nj in neighbors:
+                        if ni < 0 or nj < 0 or ni >= len(self.state) or nj >= len(self.state[i]):
                             continue
-                        if self.state[n_i][n_k] == fish:
-                            k_fish += 1
-                        elif self.state[n_i][n_k] == shrimp:
-                            k_shrimp += 1
+                        if self.state[ni][nj] == fish:
+                            n_fish += 1
+                        elif self.state[ni][nj] == shrimp:
+                            n_shrimp += 1
 
-                    if self.state[i][k] == fish:
-                        if k_fish < 2 or k_fish > 3:
-                            new_r.append(0)
+                    if self.state[i][j] == fish:
+                        if n_fish < 2 or n_fish > 3:
+                            new_row.append(0)
                         else:
-                            new_r.append(2)
-                    elif self.state[i][k] == shrimp:
-                        if k_shrimp < 2 or k_shrimp > 3:
-                            new_r.append(0)
+                            new_row.append(2)
+                    elif self.state[i][j] == shrimp:
+                        if n_shrimp < 2 or n_shrimp > 3:
+                            new_row.append(0)
                         else:
-                            new_r.append(3)
-                    elif k_fish == fish and k_shrimp == shrimp:
-                        new_r.append(2)
-                    elif k_fish == 3:
-                        new_r.append(2)
+                            new_row.append(3)
+                    elif n_fish == fish and n_shrimp == shrimp:
+                        new_row.append(2)
+                    elif n_fish == 3:
+                        new_row.append(2)
                     else:
-                        new_r.append(0)
-
-            new_state.append(new_r)
+                        new_row.append(0)
+            new_state.append(new_row)
 
         return Ocean(init_state=new_state)
-
 
 
 if __name__ == "__main__":
